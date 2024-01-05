730cbaa72c8de126191b9fc5530e55c6cc83e809
LEARN Academy <learnacademy@MacBook-Air.local>
2024-01-05 09:19:47 -0800

    api validations

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -13,21 +13,21 @@ class CarsController < ApplicarionController
         end
     end
 
-    def update
-        car = Car.find(params[:id])
-        car.update(car_params)
-        if car.valid?
-        render json: car
-        else
-            render json: car.errors, status:422
-        end
-    end
+    # def update
+    #     car = Car.find(params[:id])
+    #     car.update(car_params)
+    #     if car.valid?
+    #     render json: car
+    #     else
+    #         render json: car.errors, status:422
+    #     end
+    # end
 
-    def destroy
-        car = Car.find(params[:id])
-        car.destroy
-        render json: car
-    end
+    # def destroy
+    #     car = Car.find(params[:id])
+    #     car.destroy
+    #     render json: car
+    # end
     
     private
     def car_params
--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Car, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.create(
+    email: 'test@example.com',
+    password: 'password',
+    password_confirmation: 'password'
+  )
+}
+
+  it 'gets validate car' do 
+    car = user.car.create(
+      model: 'Tundra',
+      year: 2018,
+      color: 'black',
+      miles: '8000',
+      image: 'test.image',
+      cost:'$1000',
+      description: 'Exciting new paint job'
+    )
+    expect(car.errors[:make]).to include("can't be blank ")
+  end
 end
