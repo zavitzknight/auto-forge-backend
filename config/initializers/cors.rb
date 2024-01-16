4d03e79a817a3ce8d3f9f3ad4aae35551e30d71d
jonathanraya <jonathanraya79@gmail.com>
2024-01-16 15:56:33 -0600

    changed cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'https://auto-forge.onrender.com/'
+      origins 'https://auto-forge.onrender.com'
       resource '*',
       headers: ["Authorization"],
       expose: ["Authorization"],
